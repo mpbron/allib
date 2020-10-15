165163331aeb7220c88d5be5a4774188d7ac6434
Michiel Bron <michiel.bron@politie.nl>
2020-10-15 18:47:05 +0200

    Let the logger also work on keys only

---
+++ allib/utils/to_key.py
@@ -0,0 +1,23 @@
+from typing import TypeVar, Union, Any
+
+from ..instances import Instance
+
+KT = TypeVar("KT")
+
+def to_key(instance_or_key: Union[KT, Instance[KT, Any, Any, Any]]) -> KT:
+    """Returns the identifier of the instance if `instance_or_key` is an `Instance`
+    or return the key if `instance_or_key` is a `KT`
+
+    Parameters
+    ----------
+    instance_or_key : Union[KT, Instance]
+        An implementation of `Instance` or an identifier typed variable
+
+    Returns
+    -------
+    KT
+        The identifer of the instance (or the input verbatim)
+    """
+    if isinstance(instance_or_key, Instance):
+        return instance_or_key.identifier
+    return instance_or_key
