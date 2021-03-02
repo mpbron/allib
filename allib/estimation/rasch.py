b86c95e0e2690aa9014812320692e9e1e61e10e2
Michiel Bron <mail@michielbron.nl>
2021-03-02 18:31:54 +0100

    Rasch model combined

--- allib/estimation/rasch.py
+++ allib/estimation/rasch.py
@@ -108,6 +108,8 @@ class NonParametricRasch(RaschEstimator[KT, DT, VT, RT, LT], Generic[KT, DT, VT,
         r_script_file = os.path.join(filedir, "rasch_estimate_bootstrap.R")
         R["source"](r_script_file)
 
+    
+
     def calculate_abundance_R(self, estimator: Estimator[KT, DT, VT, RT, LT], 
                               label: LT) -> pd.DataFrame:
         df = self.get_occasion_history(estimator, label)
@@ -145,3 +147,125 @@ class ParametricRasch(NonParametricRasch[KT, DT, VT, RT, LT], Generic[KT, DT, VT
             r_df = abundance_r(df_r)
             res_df = ro.conversion.rpy2py(r_df)
         return res_df
+
+class EMRasch(NonParametricRasch[KT, DT, VT, RT, LT], Generic[KT, DT, VT, RT, LT]):
+    name = "EMRasch"
+    def __init__(self, neg_label: LT) -> None:
+        super().__init__()
+        self.neg_label = neg_label
+
+    def _start_r(self) -> None:
+        _check_R()
+        R = ro.r
+        filedir = os.path.dirname(os.path.realpath(__file__))
+        r_script_file = os.path.join(filedir, "rasch_em.R")
+        R["source"](r_script_file)
+
+    def calculate_abundance_R(self, estimator: Estimator[KT, DT, VT, RT, LT], 
+                              label: LT) -> pd.DataFrame:
+        df_pos = self.get_occasion_history(estimator, label)
+        df_neg = self.get_occasion_history(estimator, self.neg_label)
+        dataset_size = len(estimator.env.dataset)
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            df_pos_r = ro.conversion.py2rpy(df_pos)
+            df_neg_r = ro.conversion.py2rpy(df_neg)
+            abundance_r = ro.globalenv["rasch.em"]
+            r_df = abundance_r(df_pos_r, df_neg_r, dataset_size)
+            res_df = ro.conversion.rpy2py(r_df)
+        return res_df
+
+    def calculate_estimate(self, 
+                            estimator: Estimator[KT, DT, VT, RT, LT], 
+                            label: LT) -> Tuple[float, float, float]:
+        res_df = self.calculate_abundance_R(estimator, label)
+        horizon = res_df.values[0,0]
+        return horizon, horizon, horizon
+    
+    def __call__(self, 
+        learner: ActiveLearner[KT, DT, VT, RT, LT], label: LT) -> Tuple[float, float, float]:
+        if not isinstance(learner, Estimator):
+            return 0.0, 0.0, 0.0
+        estimate, lower_bound, upper_bound = self.calculate_estimate(learner, label)
+        return estimate, lower_bound, upper_bound
+
+class EMRaschCombined(NonParametricRasch[KT, DT, VT, RT, LT], Generic[KT, DT, VT, RT, LT]):
+    name = "EMRaschCombined"
+    def __init__(self, neg_label: LT) -> None:
+        super().__init__()
+        self.neg_label = neg_label
+
+    def get_occasion_history(self, 
+                             estimator: Estimator[KT, DT, VT, RT, LT], 
+                             label: LT) -> pd.DataFrame:
+        contingency_sets_pos = self.get_contingency_sets(estimator, label)
+        contingency_sets_neg = self.get_contingency_sets(estimator, self.neg_label)
+        learner_keys = union(*contingency_sets_pos.keys())
+        max_len = len(learner_keys)
+        rows_pos = {i:
+            {
+                **{
+                    f"learner_{learner_key}": int(learner_key in combination) 
+                    for learner_key in learner_keys
+                },  
+                **{
+                    "count": len(instances),
+                    "h": 1,
+                }
+            }
+            for (i, (combination, instances)) in enumerate(contingency_sets_pos.items())
+        }
+        rows_neg = {i:
+            {
+                **{
+                    f"learner_{learner_key}": int(learner_key in combination) 
+                    for learner_key in learner_keys
+                },  
+                **{
+                    "count": len(instances),
+                    "h": 0
+                }
+            }
+            for (i, (combination, instances)) in enumerate(contingency_sets_neg.items(), len(contingency_sets_pos))
+        }
+        rows = {**rows_pos, **rows_neg}
+        df = pd.DataFrame.from_dict(# type: ignore
+            rows, orient="index")
+        self.matrix_history.append(df)
+        return df
+
+    def _start_r(self) -> None:
+        _check_R()
+        R = ro.r
+        filedir = os.path.dirname(os.path.realpath(__file__))
+        r_script_file = os.path.join(filedir, "rasch_em.R")
+        R["source"](r_script_file)
+
+    def calculate_abundance_R(self, estimator: Estimator[KT, DT, VT, RT, LT], 
+                              label: LT) -> pd.DataFrame:
+        df_pos = self.get_occasion_history(estimator, label)
+        df_neg = self.get_occasion_history(estimator, self.neg_label)
+        dataset_size = len(estimator.env.dataset)
+        with localconverter(ro.default_converter + pandas2ri.converter):
+            df_pos_r = ro.conversion.py2rpy(df_pos)
+            df_neg_r = ro.conversion.py2rpy(df_neg)
+            abundance_r = ro.globalenv["rasch.em"]
+            r_df = abundance_r(df_pos_r, df_neg_r, dataset_size)
+            res_df = ro.conversion.rpy2py(r_df)
+        return res_df
+
+    def calculate_estimate(self, 
+                            estimator: Estimator[KT, DT, VT, RT, LT], 
+                            label: LT) -> Tuple[float, float, float]:
+        res_df = self.calculate_abundance_R(estimator, label)
+        horizon = res_df.values[0,0]
+        return horizon, horizon, horizon
+    
+    def __call__(self, 
+        learner: ActiveLearner[KT, DT, VT, RT, LT], label: LT) -> Tuple[float, float, float]:
+        if not isinstance(learner, Estimator):
+            return 0.0, 0.0, 0.0
+        estimate, lower_bound, upper_bound = self.calculate_estimate(learner, label)
+        return estimate, lower_bound, upper_bound
+
+
+    
