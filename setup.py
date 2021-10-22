ae1e6e15ccf7466a846dae245f7176aa5dfcb676
Michiel Bron <mail@michielbron.nl>
2021-10-22 16:21:17 +0200

    Version bump to 0.1.6.0

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setuptools.setup( # type: ignore
     name="python-allib",
-    version="0.1.5.6",
+    version="0.1.6.0",
     description="A typed Active Learning Library",
     long_description=README,
     long_description_content_type="text/markdown",
