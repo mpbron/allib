043faa7c6ce569f53da64c4a3618debcebb2d930
Michiel Bron <m.p.bron@uu.nl>
2022-01-19 17:36:13 +0100

    AP Prior distribution

--- allib/activelearning/autostop.py
+++ allib/activelearning/autostop.py
@@ -1,19 +1,28 @@
+import random
 from typing import Generic, Optional, Sequence, Tuple
 
 import instancelib as il
 import numpy as np
 from instancelib.typehints import DT, KT, LT, RT
 
-from allib.activelearning.random import RandomSampling
-import random
+from allib.utils.func import list_unzip, sort_on
 
-from allib.utils.numpy import raw_proba_chainer
-
+from ..environment.base import AbstractEnvironment
 from ..typehints import IT
 from ..typehints.typevars import VT
+from ..utils.numpy import raw_proba_chainer
 from .insclass import ILLabelProbabilityBased, ILMLBased, ILProbabilityBased
 from .poolbased import PoolBasedAL
 from .selectioncriterion import AbstractSelectionCriterion
+
+def calc_ap_prior_distribution(ranking: Sequence[Tuple[int, KT]]) -> Sequence[KT, float]:
+    ranks, keys = list_unzip(ranking)
+    ar_ranks = np.array(ranks)
+    N = len(ranking)
+    Z = np.sum(np.log(N/ar_ranks))
+    pis: np.ndarray = 1.0 / Z * np.log(N/ar_ranks)
+    return list(zip(pis.tolist(), keys))
+        
 
 
 class AutoStopLearner(PoolBasedAL[IT, KT, DT, VT, RT, LT], Generic[IT, KT, DT, RT, LT]):
@@ -33,6 +42,10 @@ class AutoStopLearner(PoolBasedAL[IT, KT, DT, VT, RT, LT], Generic[IT, KT, DT, R
         self.pos_label = pos_label
         self.neg_label = neg_label
         self.batch_size = batch_size
+
+    def __call__(self, environment: AbstractEnvironment[IT, KT, DT, VT, RT, LT]) -> PoolBasedAL[IT, KT, DT, VT, RT, LT]:
+        super().__call__(environment)
+        self.classifier.set_target_labels(self.env.labels.labelset)
     
     def update_ordering(self) -> bool:
         return True
@@ -55,8 +68,22 @@ class AutoStopLearner(PoolBasedAL[IT, KT, DT, VT, RT, LT], Generic[IT, KT, DT, R
         keys, matrix = raw_proba_chainer(raw_probas)
         return keys, matrix
 
-    def _sample(self, keys: Sequence[KT], matrix: np.ndarray) -> il.InstanceProvider[IT, KT, DT, np.ndarray, RT]:
-        pass
+    def _rank(self, provider: il.InstanceProvider[IT, KT, DT, np.ndarray, RT]) -> Sequence[Tuple[KT], float]:
+        keys, matrix = self._predict(provider)
+        pos_column = self.classifier.get_label_column_index(self.pos_label)
+        prob_vector = matrix[:,pos_column]
+        floats: Sequence[float] = prob_vector.tolist()
+        zipped = list(zip(keys, floats))
+        ranking = sort_on(1, zipped)
+        return ranking
+    
+    
+    
+    def _sample(self, ranking: Sequence[Tuple[KT, float]]) -> il.InstanceProvider[IT, KT, DT, np.ndarray, RT]:
+       
+        ranked_keys, _ = list_unzip(ranking)
+        distribution = list(enumerate(ranked_keys))
+        
 
     def __next__(self) -> IT:
         if self.batch_it == 0:
