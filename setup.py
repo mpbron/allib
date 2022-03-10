ed1ce510a2f1c9b6ca42b026e902375a4baea277
Michiel Bron <m.p.bron@uu.nl>
2022-03-10 11:40:51 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setuptools.setup( # type: ignore
     name="python-allib",
-    version="0.1.7.1",
+    version="0.1.7.2",
     description="A typed Active Learning Library",
     long_description=README,
     long_description_content_type="text/markdown",
